b265190a359d6c71341dfe62053c00c0eb7d1612
vir2s <virtualik@gmail.com>
2021-04-25 17:38:21 +0300

    urls fixed

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from courses.views import ListCourse, DetailCourse
 
 urlpatterns = [
-    path('courses/', ListCourse.as_view()),
-    path('courses/<int:pk>/', DetailCourse.as_view()),
+    path('', ListCourse.as_view()),
+    path('<int:pk>/', DetailCourse.as_view()),
 ]
--- courses_api/urls.py
+++ courses_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('courses.urls')),
+    path('api/v1/', include('courses.urls')),
 ]
