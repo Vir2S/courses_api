25bc900fee544f9c8e31df629b2d5afd029f45ce
vir2s <virtualik@gmail.com>
2021-04-25 16:14:13 +0300

    model added

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    lectures = models.IntegerField()
+
+    def __str__(self):
+        return self.title
