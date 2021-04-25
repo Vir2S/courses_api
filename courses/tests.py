7635258cf8e180c29bf95136f9a73954753b33d6
vir2s <virtualik@gmail.com>
2021-04-25 17:26:09 +0300

    test added

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from courses.models import Course
 
-# Create your tests here.
+
+class CourseTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create a course
+        test_course = Course.objects.create(
+            title='test course title',
+            description='test course description',
+            start_date='2021-04-25',
+            end_date='2021-05-05',
+            lectures='10'
+        )
+        test_course.save()
+
+    def test_course_content(self):
+        course = Course.objects.get(id=1)
+        title = f'{course.title}'
+        description = f'{course.description}'
+        start_date = f'{course.start_date}'
+        end_date = f'{course.end_date}'
+        lectures = f'{course.lectures}'
+        self.assertEqual(title, 'test course title')
+        self.assertEqual(description, 'test course description')
+        self.assertEqual(start_date, '2021-04-25')
+        self.assertEqual(end_date, '2021-05-05')
+        self.assertEqual(lectures, '10')
--- courses_api/settings.py
+++ courses_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'courses',
 ]
@@ -52,12 +53,18 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
 
 ROOT_URLCONF = 'courses_api.urls'
 
